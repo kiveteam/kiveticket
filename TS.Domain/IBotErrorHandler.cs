3667afbd6f6868eb36b5273fc72efd3b65704a00
mds <mds@etz-vektor.ru>
2024-11-14 10:40:08 +0400

    Рефакторинг

---
+++ TS.Domain/IBotErrorHandler.cs
@@ -0,0 +1,8 @@
+﻿using Telegram.Bot;
+
+namespace TS.Domain;
+
+public interface IBotErrorHandler
+{
+    Task Handle(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken);
+}
---
+++ TS.Domain/IBotUpdateHandler.cs
@@ -0,0 +1,10 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TS.Domain;
+
+public interface IBotUpdateHandler
+{
+    void Initialize(DateTime startedDate);
+    Task Handle(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken);
+}
